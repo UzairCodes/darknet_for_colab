5b17db8f82b1d8e63f3e4e9f9607d4d6d2fdb6bb
UzairCodes <81608902+UzairCodes@users.noreply.github.com>
2021-03-30 10:40:11 +0500

    gc10_det specs

--- yolov4_config.py
+++ yolov4_config.py
@@ -11,13 +11,13 @@ and cfg/yolov4_custom_test.cfg
 
 """
 
-classes=4
+classes=10
 max_batches=8000
 batch=64
 subdivisions=16
-width=416
-height=416
-channels=3
+width=224
+height=224
+channels=1
 momentum=0.949
 decay=0.0005
 learning_rate=0.001
